adc6555e956ab4624460ff218c6623303a1cc898
zeynepbaharbaksi <zbbaksi@gmail.com>
2026-03-22 11:04:03 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
+  typescript: {
+    ignoreBuildErrors: true,
   },
   async headers() {
     return [
