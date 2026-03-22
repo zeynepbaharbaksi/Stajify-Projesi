d7250cbf0811a93727b693f9e8188f7bd240c62e
zeynepbaharbaksi <zbbaksi@gmail.com>
2026-03-22 10:47:06 +0300

    next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   async headers() {
     return [
       {
